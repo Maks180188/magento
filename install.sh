2a78cb46bb21c81887b9b98c79560a0946de5228
maksim <mgalkun@gmail.com>
2021-07-06 09:49:44 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+bin/magento setup:install \
+--base-url=http://magento \
+--db-host=localhost \
+--db-name=magento \
+--db-user=admin \
+--db-password=123456 \
+--admin-firstname=admin \
+--admin-lastname=admin \
+--admin-email=admin@admin.com \
+--admin-user=admin \
+--admin-password=admin123 \
+--language=en_US \
+--currency=USD \
+--timezone=America/Chicago \
+--use-rewrites=1
